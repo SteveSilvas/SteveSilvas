dbd55e52f0839905eb64008c77a79d41758d013d
Steve Silva <steve.silva@gpssa.com.br>
2024-08-04 13:41:15 -0300

    Adicionando logo como icone de navegação para home no menu.

--- src/Components/Navbar/NavItem.tsx
+++ src/Components/Navbar/NavItem.tsx
@@ -3,13 +3,15 @@ import { Link } from 'react-router-dom';
 import "./styles.css";
 interface INavItem {
     path: string;
-    title: string;
+    title?: string;
+    children?: React.ReactNode;
     onClick: () => void;
 }
-const NavItem: React.FC<INavItem> = ({ path, title, onClick }) => {
+const NavItem: React.FC<INavItem> = ({ path, title, onClick, children }) => {
     return (
         <Link to={path} className='NavItem' onClick={onClick}>
             {title}
+            {children}
         </Link>
     );
 }
--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import NavItem from './NavItem';
 import "./styles.css";
-
+import logo from '../../../public/S.png';
 const Navbar: React.FC = () => {
     const [menuVisible, setMenuVisible] = useState(false);
 
@@ -9,18 +9,26 @@ const Navbar: React.FC = () => {
         setMenuVisible(!menuVisible);
     };
 
-    const classe = `${menuVisible ? 'NavBarMobile': 'Navbar'} `;
+    const classe = `${menuVisible ? 'NavBarMobile' : 'Navbar'} `;
 
     return (
         <>
             <nav className={classe}>
-                <NavItem path='/about' title='Sobre' onClick={toggleMenu}/>
-                <NavItem path='/projects' title='Projetos' onClick={toggleMenu}/>
-                <NavItem path='/training' title='Formação' onClick={toggleMenu}/>
-                <NavItem path='/certificate' title='Certificados' onClick={toggleMenu}/>
-                <NavItem path='/experience' title='Experiências' onClick={toggleMenu}/>
-                <NavItem path='/skills' title='Habilidades' onClick={toggleMenu}/>
-                <NavItem path='/contact' title='Contatos' onClick={toggleMenu}/>
+                <NavItem
+                    path='/'
+                    children={
+                        <img
+                            className='NavLogo'
+                            src={logo}
+                            alt='Steve Silva' />}
+                    onClick={toggleMenu} />
+                <NavItem path='/about' title='Sobre' onClick={toggleMenu} />
+                <NavItem path='/projects' title='Projetos' onClick={toggleMenu} />
+                <NavItem path='/training' title='Formação' onClick={toggleMenu} />
+                <NavItem path='/certificate' title='Certificados' onClick={toggleMenu} />
+                <NavItem path='/experience' title='Experiências' onClick={toggleMenu} />
+                <NavItem path='/skills' title='Habilidades' onClick={toggleMenu} />
+                <NavItem path='/contact' title='Contatos' onClick={toggleMenu} />
             </nav>
             <button className="hamburger-button" onClick={toggleMenu}>
                 ☰
