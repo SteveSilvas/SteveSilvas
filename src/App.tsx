605e8231f877204f73f4acd9e14a144016a6e97f
Steve Silva <steve.silva@gpssa.com.br>
2024-08-04 13:17:49 -0300

    Removendo header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import './App.css';
-import Header from './Components/Header';
+import Navbar from './Components/Navbar';
 
 function App() {
     return (
         <div className="App">
-            <Header />
+            <Navbar />
             <div className='PageGrid'>
                 <Outlet />
             </div>
